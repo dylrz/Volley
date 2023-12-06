657515827276f4c33ef9f54e9a158617fc0e7ca1
Dylan <dylanrzhai@gmail.com>
2023-12-06 12:53:24 -0800

    Nice stuff

--- backend/routes/mainRoutes.js
+++ backend/routes/mainRoutes.js
@@ -8,25 +8,6 @@ router.get("/", function (req, res) {
     res.render("index")
 });
 
-// router.get('/main', async (req, res) => {
-//     const username = req.session.username || 'Guest';
-  
-//     const userId = req.user ? req.user._id : null;
-  
-//     try {
-//       // fetch all teams associated with the user
-//       const teams = userId ? await Team.find({ user: userId }) : [];
-  
-//       res.render('main', {
-//         name: username,
-//         teams: teams
-//       });
-//     } catch (error) {
-//       console.error("Failed to fetch the user's teams:", error);
-//       res.status(500).send("Error fetching teams information");
-//     }
-//   });
-
 router.get('/main', async (req, res) => {
   const username = req.session.username || 'Guest';
   const userId = req.user ? req.user._id : null;
---
+++ backend/views/Login.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement your login logic here
+    // You can check the username and password, and navigate to the homepage if login is successful.
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Username:</Text>
+      <TextInput
+        style={styles.input}
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+      />
+      <Text style={styles.label}>Password:</Text>
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  input: {
+    width: 200,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+});
+
+export default Login;
